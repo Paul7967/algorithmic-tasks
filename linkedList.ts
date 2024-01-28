364248ea4dcfe72097497f507c17aaae3fa5faf3
Pavel Ostatochnikov <postatochnikov@yandex.ru>
2024-01-28 18:38:24 +0500

    Add TS and tsconfig.json

--- linkedList.ts
+++ linkedList.ts
@@ -1,21 +1,50 @@
 class LinkedListNode {
-    constructor() {
-        this.value = value;
+    constructor(val: typeof LinkedListNode.prototype.val, next = null) {
+        this.val = val;
         this.next = null;
     }
 
+    val: number;
+    next: {} | null;
+
     toString() {
-        return `${this.value}`;
+        return `${this.val}`;
     }
 }
 
 class LinkedList {
+    head: LinkedListNode | null;
+    tail: LinkedListNode | null;
+
     constructor() {
         this.head = null;
         this.tail = null;
+    }
+
+    append(val: typeof LinkedListNode.prototype.val) {
+        const newNode = new LinkedListNode(val);
+
+        if (!this.head || !this.tail) {
+            this.head = newNode;
+            this.tail = newNode;
+
+            return this;
+        }
+
+        this.tail.next = newNode;
+        this.tail = newNode;
     }
 }
 
 export const makeSingleLinkedListFromArray = (inArr: []) => {
     const result = {};
 };
+
+const list = new LinkedList();
+
+list.append(1);
+list.append(2);
+list.append(3);
+list.append(4);
+list.append(5);
+console.log(list);
