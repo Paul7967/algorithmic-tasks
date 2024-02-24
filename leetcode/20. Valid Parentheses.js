b263c6a364326b1e1e83b87408bd0cfadd868058
Pavel Ostatochnikov <postatochnikov@yandex.ru>
2024-02-24 11:20:31 +0500

    Edit jest.config

--- leetcode/20. Valid Parentheses.js
+++ leetcode/20. Valid Parentheses.js
@@ -5,7 +5,6 @@
 // Open brackets must be closed by the same type of brackets.
 // Open brackets must be closed in the correct order.
 // Every close bracket has a corresponding open bracket of the same type.
- 
 
 // Example 1:
 
@@ -19,7 +18,6 @@
 
 // Input: s = "(]"
 // Output: false
- 
 
 // Constraints:
 
@@ -30,38 +28,43 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
+var isValid = function (s) {
     const checkCouple = (leftSimbol, rightSimbol) => {
-        if (leftSimbol==="(" && rightSimbol===")") return true;
-        if (leftSimbol==="{" && rightSimbol==="}") return true;
-        if (leftSimbol==="[" && rightSimbol==="]") return true;
+        if (leftSimbol === "(" && rightSimbol === ")") return true;
+        if (leftSimbol === "{" && rightSimbol === "}") return true;
+        if (leftSimbol === "[" && rightSimbol === "]") return true;
         return false;
     };
-    
+
     const checkLeftBracket = (inSimbol) => {
-        if (inSimbol==="{" || inSimbol==="[" || inSimbol === "(") {
+        if (inSimbol === "{" || inSimbol === "[" || inSimbol === "(") {
             return true;
         }
         return false;
     };
 
-    if (s.length%2 !==0) return false;
+    if (s.length % 2 !== 0) return false;
 
     const leftBrackets = [];
 
     for (let i = 0; i < s.length; i++) {
-        if (checkLeftBracket(s[i])) {    // если левая скобка добавляем ее в массив
+        if (checkLeftBracket(s[i])) {
+            // если левая скобка добавляем ее в массив
             leftBrackets.push(s[i]);
-        } else { // если правая скобка
-            if (leftBrackets.length && checkCouple(leftBrackets[leftBrackets.length-1], s[i])) {
+        } else {
+            // если правая скобка
+            if (
+                leftBrackets.length &&
+                checkCouple(leftBrackets[leftBrackets.length - 1], s[i])
+            ) {
                 leftBrackets.pop();
             } else {
                 return false;
             }
-        } 
+        }
     }
 
-    return leftBrackets.length===0;
+    return leftBrackets.length === 0;
 };
 
 // console.log(isValid("())"));
@@ -71,4 +74,3 @@ var isValid = function(s) {
 // console.log(isValid("[{}()]"));
 // console.log(isValid("[()]"));
 console.log(isValid("(("));
-
